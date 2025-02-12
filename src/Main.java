2fa8472e3d4e5a4ba8db45b09cdda467031f9f8e
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-02-12 14:09:37 +0100

    Aprendiendo ha crear tests

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,21 @@ public class Main {
         Personaje personaje2 = new Personaje("Vegeta");//crea un personaje en el cual tú tienes que meter los datos en la main
         System.out.println("hola "+ personaje1.nome);
         System.out.println("hola " +personaje2.nome);
+
         int una_variable = personaje1.perderVida(15);
+        if(una_variable == 985) System.out.println("Daño quitado ");
+        else System.out.println("no furrula");
+
         System.out.println("vidas personaje 1: "+ personaje1.getVidas());
         System.out.println("Vidas Personaje 1: "+ una_variable);
+
+        //test1
+        personaje1.setVidas(1000);
+        int danho = 100;
+        int respuesta_esperada = 900;
+        int vidas_restantes = personaje1.perderVida(danho);
+        if(vidas_restantes == respuesta_esperada){System.out.println("ok");} else System.out.println("error");
+
+
     }//end main
 }//end class

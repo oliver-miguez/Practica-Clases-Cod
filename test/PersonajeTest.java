10d5d5de3bbb9cf6ba453b0d3d307fd00d19745a
Oliver Miguez Alonso <omiguezalonso@danielcastelao.org>
2025-02-14 09:05:25 +0100

    añadir algunos espacios

--- test/PersonajeTest.java
+++ test/PersonajeTest.java
@@ -10,5 +10,9 @@ class PersonajeTest {
         p.setVidas(1000);
         int daño = 100;
         assertEquals(900,p.perderVida(daño)); // 900 es el valor esperado , y p.vidas es el valor que se espera que sea 900 sabiendo que le quitan 100 de daño
-    }
-}
+
+
+
+
+    }//end void
+}//end class
